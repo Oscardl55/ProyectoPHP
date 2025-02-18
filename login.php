edf751632ebdee34c9f4a85f93b2dc72e3536be0
Ivanchii7 <ivanromaroma@gmail.com>
2025-02-18 16:57:45 +0100

    Login php

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($username === 'admin' && $password === 'password') {
         $_SESSION['loggedin'] = true;
         $_SESSION['username'] = $username;
-        header("Location: index.html");
+        header("Location: html/index.html");
         exit;
     } else {
         $error = "Usuario o contraseña incorrectos";
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/login.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Login</title>
 </head>
